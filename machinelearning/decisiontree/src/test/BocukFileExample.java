94830126c6f1a68df528d33fdc9bf753961ba2ba
Gizil Oguz <gizil.oguz@gmail.com>
2008-03-30 02:53:26 +0000

    new file system
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/contrib@19319 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ machinelearning/decisiontree/src/test/BocukFileExample.java
@@ -0,0 +1,50 @@
+package test;
+
+import java.util.List;
+
+import dt.DecisionTree;
+import dt.builder.IDTreeBuilder;
+import dt.memory.FactSetFactory;
+import dt.memory.WorkingMemory;
+import dt.tools.RulePrinter;
+
+public class BocukFileExample {
+
+	public static void main(String[] args) {
+		
+
+		WorkingMemory simple = new WorkingMemory();
+
+
+		/* insert the guys */
+		//String klassCar =  FactSetFactory.insertCarSet(simple);
+		String klassNursery =  FactSetFactory.insertNurserySet(simple);
+		
+		//String klassAdvertisement = FactSetFactory.insertAdvertisementSet(simple);
+
+		
+		boolean buildTree = true;
+		if (buildTree) {
+
+			IDTreeBuilder bocuk = new IDTreeBuilder();
+			//DecisionTreeBuilderMT bocuk = new DecisionTreeBuilderMT();
+
+			long dt = System.currentTimeMillis();
+			//DecisionTree bocuksTree = bocuk.build(simple, klassCar, "classCar", null);
+			DecisionTree bocuksTree = bocuk.build(simple, klassNursery, "classnursery", null);
+			
+			//DecisionTree bocuksTree = bocuk.build(simple, klassAdvertisement, "classAdvertisement", FactSetFactory.attributesOfAdvertisement);
+			
+			
+			dt = System.currentTimeMillis() - dt;
+			System.out.println("Time"+dt + " facts read: "+bocuksTree.getNumRead() + " num call: "+ bocuk.getNumCall() );
+			//System.out.println(bocuksTree);
+
+			RulePrinter my_printer = new RulePrinter();
+			my_printer.printer(bocuksTree, null, null);
+		}
+	}
+
+
+}
+
---
+++ machinelearning/decisiontree/src/test/BocukObjectExample.java
@@ -0,0 +1,51 @@
+package test;
+
+import java.util.ArrayList;
+
+import dt.DecisionTree;
+import dt.builder.IDTreeBuilder;
+import dt.memory.OOFactSet;
+import dt.memory.WorkingMemory;
+import dt.tools.RulePrinter;
+
+public class BocukObjectExample {
+
+	public static void main(String[] args) {
+		RestaurantOld arest = new RestaurantOld(true, false,  false,  true, "Full", 1,   false,   false,  "Thai",    "30-60",  false);
+		Class<?> k = arest.getClass();
+		ArrayList<Object> facts = new ArrayList<Object>();
+		facts.add(new RestaurantOld(true, false,  false,  true, "Full", 1,   false,   false,  "Thai",    "30-60",  false));
+		facts.add(new RestaurantOld(false,  true, false,  false,  "Some", 1,   false,   false,  "Burger",  "0-10",   true));
+		facts.add(new RestaurantOld(true, false,  true, true, "Full", 1,   true,   false,  "Thai",    "10-30",  true));
+		facts.add(new RestaurantOld(true, false,  true, false,  "Full", 3, false,   true, "French",  ">60",    false)); 
+		facts.add(new RestaurantOld(false,  true, false,  true, "Some", 2,  true,  true, "Italian", "0-10",   true));
+		facts.add(new RestaurantOld(false,  true, false,  false,  "None", 1,   true,  false,  "Burger",  "0-10",   false));
+		facts.add(new RestaurantOld(false,  false,  false,  true, "Some", 2,  true,  true, "Thai",    "0-10",   true));
+		facts.add(new RestaurantOld(false,  true, true, false,  "Full", 1,   true,  false,  "Burger",  ">60",    false)); 
+		facts.add(new RestaurantOld(true, true, true, true, "Full", 3, false,   true, "Italian", "10-30",  false)); 
+		facts.add(new RestaurantOld(false,  false,  false,  false,  "None", 1,   false,   false,  "Thai",    "0-10",   false)); 
+		facts.add(new RestaurantOld(true, true, true, true, "Full", 1,   false,   false,  "Burger",  "30-60",  true));
+
+		WorkingMemory simple = new WorkingMemory();
+		OOFactSet fs = simple.getFactSet(arest.getClass());
+
+		for(Object r: facts) {
+			try {
+				//simple.insert(element)
+				fs.insert(r);
+			} catch (Exception e) {
+				System.out.println("Inserting element "+ r + " and "+ e);
+			}
+		}
+		
+		IDTreeBuilder bocuk = new IDTreeBuilder();
+		
+		long dt = System.currentTimeMillis();
+		DecisionTree bocuksTree = bocuk.build(simple, k, "will_wait", null);
+		dt = System.currentTimeMillis() - dt;
+		System.out.println("Time"+dt+"\n"+bocuksTree);
+		
+		RulePrinter my_printer = new RulePrinter();
+		my_printer.printer(bocuksTree,"id3" , new String("src/id3/rules"+".drl"));
+	}
+}
