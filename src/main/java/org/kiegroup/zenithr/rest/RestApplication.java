3895c153b0de383cd3f1e83428394d28153f70a9
Babak Mozaffari <bmozaffa@redhat.com>
2019-01-20 16:41:04 -0800

    Initial commit, HelloWorld JAX-RS Thorntail service
    Signed-off-by: Babak Mozaffari <bmozaffa@redhat.com>

---
+++ src/main/java/org/kiegroup/zenithr/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package org.kiegroup.zenithr.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+
+}
