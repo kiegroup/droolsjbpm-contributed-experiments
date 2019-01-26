c3d70dc1c47a15cd879321df1bdde0e7a29cb6fc
Babak Mozaffari <bmozaffa@redhat.com>
2019-01-25 19:05:41 -0800

    API added with operator sdk version "v0.4.0+git": operator-sdk add api --api-version=zenithr.kiegroup.org/v1 --kind=ZenithrApp
    Signed-off-by: Babak Mozaffari <bmozaffa@redhat.com>

---
+++ pkg/apis/zenithr/v1/zenithrapp_types.go
@@ -0,0 +1,45 @@
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ZenithrAppSpec defines the desired state of ZenithrApp
+type ZenithrAppSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+}
+
+// ZenithrAppStatus defines the observed state of ZenithrApp
+type ZenithrAppStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ZenithrApp is the Schema for the zenithrapps API
+// +k8s:openapi-gen=true
+type ZenithrApp struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ZenithrAppSpec   `json:"spec,omitempty"`
+	Status ZenithrAppStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ZenithrAppList contains a list of ZenithrApp
+type ZenithrAppList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ZenithrApp `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ZenithrApp{}, &ZenithrAppList{})
+}
