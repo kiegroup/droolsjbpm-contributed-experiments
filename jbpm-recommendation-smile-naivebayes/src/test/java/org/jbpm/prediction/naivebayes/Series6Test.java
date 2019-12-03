bd8139664add75913b5c71323155b0c02965c757
Rui  Vieira <ruidevieira@googlemail.com>
2019-12-03 14:31:33 +0000

    Increased data uncertainty and lowered confidence threshold for auto-completion test

--- jbpm-recommendation-smile-naivebayes/src/test/java/org/jbpm/prediction/naivebayes/Series6Test.java
+++ jbpm-recommendation-smile-naivebayes/src/test/java/org/jbpm/prediction/naivebayes/Series6Test.java
@@ -27,11 +27,13 @@ import static org.junit.Assert.assertTrue;
 public class Series6Test extends NaiveBayesRecommendationServiceProcessTest {
     @Test
     public void testAutoCompletion() {
-        System.setProperty("org.jbpm.task.prediction.service.confidence_threshold", "0.95");
+        System.setProperty("org.jbpm.task.prediction.service.confidence_threshold", "0.85");
         Map<String, Object> outputs = new HashMap<>();
-        startAndReturnTaskOutputData("item6", "mary", 5, false);
         for (int i = 0; i < 200; i++) {
-            outputs = startAndReturnTaskOutputData("item6", "mary", 5, true);
+                startAndReturnTaskOutputData("item6", "mary", 5, false);
+            for (int j = 0 ; j < 6 ; j++) {
+                outputs = startAndReturnTaskOutputData("item6", "mary", 5, true);
+            }
         }
         assertTrue(outputs.isEmpty());
     }
